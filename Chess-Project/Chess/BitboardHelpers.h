52a7a8f5e20d689e0e8550afee045f04f49bdd7f
Zofikar <pawel.chwalczyk@gmail.com>
2024-06-20 03:27:40 +0200

    separated BitboardHelpers into .h and .cpp file

--- Chess-Project/Chess/BitboardHelpers.h
+++ Chess-Project/Chess/BitboardHelpers.h
@@ -1,72 +1,22 @@
 #pragma once
-#include<array>
-#include<vector>
-#include<stdexcept>
+
+#include <cstdint>
+#include <vector>
+#include <stdexcept>
 #include <intrin.h>
 
 using Bitboard = uint64_t;
 
-class BitboardHelpers
-{
+class BitboardHelpers {
 public:
-    static inline bool GetBit(const Bitboard& bb, unsigned int square) {
-        return static_cast<bool>((bb>>square)&1);
-    }
-
-    static inline void SetBit(Bitboard& bb, unsigned int square) {
-        bb |= 1ull << square;
-    }
-
-    static inline void ToggleBit(Bitboard& bb, unsigned int square) {
-        bb ^= 1ull << square;
-    }
-
-    static inline void ClearBit(Bitboard& bb, unsigned int square) {
-        bb &= ~(1ull << square);
-    }
-
-    static inline unsigned int GetIndexOfLSB(const Bitboard& bb) {
-        if (!bb) throw std::invalid_argument("Board is empty");
-        unsigned long index;
-        _BitScanForward64(&index, bb);
-        return static_cast<unsigned int>(index);
-    }
-
-    static inline unsigned int GetAndClearIndexOfLSB(Bitboard& bb) {
-        if (!bb) throw std::invalid_argument("Board is empty");
-        unsigned long index;
-        _BitScanForward64(&index, bb);
-        ClearBit(bb, index);
-        return static_cast<unsigned int>(index);
-    }
-
-    static inline unsigned int GetIndexOfFSB(const Bitboard& bb) {
-        if (!bb) throw std::invalid_argument("Board is empty");
-        unsigned long index;
-        _BitScanReverse64(&index, bb);
-        return static_cast<unsigned int>(index);
-    }
-
-    static inline unsigned int GetAndClearIndexOfFSB(Bitboard& bb) {
-        if (!bb) throw std::invalid_argument("Board is empty");
-        unsigned long index;
-        _BitScanReverse64(&index, bb);
-        ClearBit(bb, index);
-        return static_cast<unsigned int>(index);
-    }
-
-    static inline std::vector<unsigned int> GetAllBits(Bitboard bb) {
-        std::vector<unsigned int> bits;
-        for (int i = 0; i < 64; i++) {
-            if (static_cast<bool>(GetBit(bb, i)))
-                bits.push_back(i);
-        }
-        return bits;
-    }
-
-    static inline unsigned int GetNumOfBitsSet(Bitboard bb) {
-        return __popcnt64(bb);
-    }
-
+    static bool GetBit(const Bitboard& bb, unsigned int square);
+    static void SetBit(Bitboard& bb, unsigned int square);
+    static void ToggleBit(Bitboard& bb, unsigned int square);
+    static void ClearBit(Bitboard& bb, unsigned int square);
+    static unsigned int GetIndexOfLSB(const Bitboard& bb);
+    static unsigned int GetAndClearIndexOfLSB(Bitboard& bb);
+    static unsigned int GetIndexOfFSB(const Bitboard& bb);
+    static unsigned int GetAndClearIndexOfFSB(Bitboard& bb);
+    static std::vector<unsigned int> GetAllBits(Bitboard bb);
+    static unsigned int GetNumOfBitsSet(Bitboard bb);
 };
-
